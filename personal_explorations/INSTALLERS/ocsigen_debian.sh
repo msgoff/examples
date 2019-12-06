ad04f6ed7c0503baf20b780e5de2567782c80dab
mike <mike@lcd-outlet.com>
2019-12-06 12:00:13 -0800

    ocsigen test

---
+++ personal_explorations/INSTALLERS/ocsigen_debian.sh
@@ -0,0 +1,46 @@
+ sudo apt install postgresql ruby-sass
+ sudo apt install pkg-config
+ sudo apt install libgmp-dev
+ sudo apt install ocaml
+ sudo apt install opam
+ sudo apt install libgdbm-dev
+ sudo apt install bubblewrap
+ sudo apt install ocaml
+ sudo apt install opam
+ sudo apt upgrade opam
+ sudo apt install git m4 rsync mercurial darcs
+ sudo apt install ocaml
+ sudo apt install zlib1g-dev libssl-dev libpcre3-dev
+ sudo apt install ocsigen
+ sudo apt install ocsigen-server
+ sudo apt install ocsigenserver
+ sh <(curl -sL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
+ opam init
+ opam switch create 4.03.0
+ sudo apt upgrade
+ sudo apt update
+ sudo apt upgrade
+ opam update
+ opam upgrade
+ opam init
+ opam install ocsigen-start
+ opam init
+ opam switch create 4.03.0
+ opam install ocsigen-start
+ eliom-distillery -name myapp -template os.pgocaml
+ sudo apt install eliom
+ eliom-distillery -name myapp -template os.pgocaml
+ eliom-distillery -name myapp -template os.pgocaml
+ eliom-distillery -name myapp -template basic os.pgocaml
+ eliom-distillery -name myapp -template basic .
+ eliom-distillery -name myapp -template basic
+ ls
+ cd myapp/
+ ls
+ make
+ vim README 
+ sudo make install
+ vim README 
+ sudo make run
+ sudo make run.byte
+ ls
