6421e784a47c34d121d96cda37c8e8070d38aadc
mike <mike@lcd-outlet.com>
2019-12-29 19:15:29 +0100

    jupyter install ocaml

--- personal_explorations/INSTALLERS/3_jupyter.sh
+++ personal_explorations/INSTALLERS/3_jupyter.sh
@@ -1,6 +1,10 @@
 sudo apt install libzmq3-dev
-virtualenv /home/user/venv --python=python3.7
-source /home/user/venv/bin/activate
+sudo apt install libgmp-dev
+sudo apt install python3-pip
+user_name="$(whoami)"
+python_version="$(find /usr/local/lib/ -type f|grep python3|cut -d '/' -f 5|sort|uniq|tail -n 1)"
+virtualenv /home/"$user_name"/venv --python="$python_version"
+source /home/"$user_name"/venv/bin/activate
 pip install jupyter
 opam install cohttp-async
 opam install cohttp
