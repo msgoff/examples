3c5dc0915008953e5f9f8ceeb2ad1e4d1be84d42
mike <mike@lcd-outlet.com>
2019-12-30 15:13:49 +0100

    clean up files

--- personal_explorations/INSTALLERS/2_merlin.sh
+++ personal_explorations/INSTALLERS/2_merlin.sh
@@ -7,14 +7,18 @@ opam install user-setup
 opam install merlin
 opam install core
 opam install camlp4
+opam install core_extended
+opam install core_bench
+opam install async
+opam install camlp5
 eval $(opam config env)
-opam-user-setup install 
+opam-user-setup install
 
 echo "let g:opamshare = substitute(system('opam config var share'),'\n$','','''')" >> ~/.vimrc
 
 echo 'execute "set rtp+=" . g:opamshare . "/merlin/vim"' >> ~/.vimrc
 
 if [[  -f ~/.ocamlinit ]];
-then 
+then
 	cp ocamlinit ~/.ocamlinit
 fi
